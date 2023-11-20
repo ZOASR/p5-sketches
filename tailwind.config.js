f8e545eb866749899ebbda03cd6c0f93743ccda0
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-11-20 19:24:37 +0200

    modified shadow md size for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				md: "0 4px 6px -1px rgb(0 0 0 / 0.5), 0 2px 4px -2px rgb(0 0 0 / 0.5);",
+			},
+		},
 	},
 	plugins: [],
 };
