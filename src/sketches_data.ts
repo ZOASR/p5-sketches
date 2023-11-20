d45108832521a1059df3ff8e876d530ec05c28a6
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-11-20 19:33:24 +0200

    Moved fonts and media from public folder to root path to satisy vite's specifications

--- src/sketches_data.ts
+++ src/sketches_data.ts
@@ -10,7 +10,7 @@ export const sketches: Sketch[] = [
 		title: "Flock",
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		imageUrl: "../../../public/images/flock_thumb.png",
+		imageUrl: "../../../images/flock_thumb.png",
 		sourceCode:
 			"https://github.com/ZOASR/p5-sketches/blob/main/src/Fractal/sketch.ts",
 	},
@@ -18,7 +18,7 @@ export const sketches: Sketch[] = [
 		title: "Fractal",
 		description:
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magnaaliqua. Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat.",
-		imageUrl: "../../../public/images/fractal_thumb.png",
+		imageUrl: "../../../images/fractal_thumb.png",
 		sourceCode:
 			"https://github.com/ZOASR/p5-sketches/blob/main/src/Flock/sketch.ts",
 	},
