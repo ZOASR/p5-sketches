8b6d221eb5b54bb0a3b4b1e8879128817eb6859d
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-11-21 15:52:41 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 const Header = ({ title }: { title: string }) => {
 	return (
-		<div className="header sticky z-10 top-0 flex justify-center w-full p-4">
+		<header className="header sticky z-10 top-0 flex justify-center w-full p-4">
 			{title ? (
 				<h1 className="text-3xl font-extrabold">{title}</h1>
 			) : (
 				<h1 className="text-3xl font-extrabold">p5 Sketches</h1>
 			)}
-		</div>
+		</header>
 	);
 };
 
