8ff12fbd0497c6c6ed3879390593e56715b414f5
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-11-25 14:32:03 +0200

    Modified the header content and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,28 @@ import githubIcon from "../../images/github-mark-white.svg";
 const Header = ({ title }: { title: string }) => {
 	return (
 		<header className="header sticky z-10 top-0 h-min flex align-middle justify-between w-full p-4">
-			{title ? (
-				<h1 className="text-3xl font-extrabold">{title}</h1>
-			) : (
-				<h1 className="text-3xl font-extrabold">p5 Sketches</h1>
-			)}
-			<nav className="flex gap-5  justify-self-end">
-				<Link
-					className=" opacity-25 hover:underline hover:opacity-100 transition-all duration-150 text-center"
-					to="/"
-				>
-					Sketches
-				</Link>
-				<Link
-					className="opacity-25 hover:opacity-100 transition-all duration-150"
-					to="https://github.com/ZOASR/p5-sketches/"
-					target="_blank"
-				>
-					<img src={githubIcon} width={30} alt="" />
-				</Link>
-			</nav>
+			<div className="flex justify-between mx-auto w-full sm:w-1/2">
+				{title ? (
+					<h1 className="text-3xl font-extrabold">{title}</h1>
+				) : (
+					<h1 className="text-3xl font-extrabold">p5 Sketches</h1>
+				)}
+				<nav className="flex gap-5  justify-self-end">
+					<Link
+						className=" opacity-25 hover:underline hover:opacity-100 transition-all duration-150 text-center"
+						to="/"
+					>
+						Sketches
+					</Link>
+					<Link
+						className="opacity-25 hover:opacity-100 transition-all duration-150"
+						to="https://github.com/ZOASR/p5-sketches/"
+						target="_blank"
+					>
+						<img src={githubIcon} width={30} alt="" />
+					</Link>
+				</nav>
+			</div>
 		</header>
 	);
 };
